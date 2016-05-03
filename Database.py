eb9b664f4698b18b89b5fdd3c69356e5f21aebe1
tanghanzxp <tanghanzxp@gmail.com>
2016-05-03 10:27:35 +0800

    Second Commit , test

--- Database.py
+++ Database.py
@@ -3,20 +3,51 @@
 #time = 2016.04.24
 #  #author = 'tanghan'
 
-from sqlalchemy import String,Column,create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, create_engine,Integer
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 '''医学图片分类别有'''
 
 #数据库基类
 Base = declarative_base()
-
+engine = create_engine('mysql+mysqlconnector://root:tanghan@localhost:3306/Disease_Pics')
 #定义数据库对象
+class Heading(Base):
+    #表明
+    __tablename__ = 'Heading'
 
+
+    id = Column(Integer,autoincrement=True,primary_key=True)
+    local_addr = Column(String(255),nullable=False)
+    net_addr = Column(String(255),nullable=False)
+
+class Lung(Base):
+    # 表明
+    __tablename__ = 'Heading'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    local_addr = Column(String(255), nullable=False)
+    net_addr = Column(String(255), nullable=False)
+
+class Pupil(Base):
+    # 表明
+    __tablename__ = 'Heading'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    local_addr = Column(String(255), nullable=False)
+    net_addr = Column(String(255), nullable=False)
+
+class Abdomen(Base):
+    # 表明
+    __tablename__ = 'Heading'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    local_addr = Column(String(255), nullable=False)
+    net_addr = Column(String(255), nullable=False)
 
 
 
 #初始化数据库连接
-engine = create_engine('mysql+mysqlconnector://root:tanghan@localhost:3306/Disease_Pics')
+
 DBSession = sessionmaker(bind=engine)
--- Picture_Crawel.py
+++ Picture_Crawel.py
@@ -2,6 +2,10 @@
 #file is used to grap picture and their figure or paper from some specific websits
 #author = tanghan
 
+import bs4
+from selenium import webdriver
+
+
 #文本分析类，用于大致判断图片类别
 class Text_Parser:
     def __init__(self,webtext):
@@ -12,3 +16,25 @@ class Crawel:
     def __init__(self):
         print ('Start Grap')
 
+root = 'http://emedicine.medscape.com'
+class MedCrawel:
+    def __init__(self):
+        self.__root = 'http://reference.medscape.com/guide/anatomy'
+        self.driver = webdriver.PhantomJS()
+        self.record = []
+
+    def start(self):
+        print 1
+        self.driver.get(self.__root)
+        print 2
+        soup = bs4.BeautifulSoup(self.driver.page_source,'html.parser')
+        print 3
+        target_div = soup.find(attrs={'id': 'subdiralphalist'})
+        print 4
+        links = target_div.find_all('a');
+        print 5
+        for link in links:
+            # self.record.append(root+link['href'])
+            print link['href']
+test = MedCrawel()
+test.start()

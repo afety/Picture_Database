a97c2ddac0f04e74948cc5f552f4965a3241b19f
tanghanzxp <tanghanzxp@gmail.com>
2016-05-06 17:58:32 +0800

    Second Commit , test

--- Split.py
+++ Split.py
@@ -5,15 +5,18 @@ import nltk
 import threading
 #单词拼写检查
 import enchant
+
 import re
 
 
 anthor = 'tanghan'
 
-FEATHER_WORDS = []
 Stemming = nltk.PorterStemmer()
 spellcheck = enchant.Dict("en_US")
-JUDGE = []
+FEATHER_WORDS = []
+for x in xrange(26):
+    FEATHER_WORDS.append([])
+COUNTER = []
 
 def getPos(char):
     if ord(char) < 97 or ord(char) > 122:
@@ -30,43 +33,54 @@ def WordExtra(filename):
             text += line[:len(line)-1]+" "
         res = nltk.word_tokenize(text)
         for x in xrange(len(res)):
-            if len(res[x]) >= 4 and spellcheck.check(res[x]):
+            if len(res[x]) >= 4 and spellcheck.check(res[x]) and res[x].isalpha():
                 res[x] = res[x].lower()
                 vocau[getPos(res[x][0])].append(res[x])
-    print vocau
     return vocau
 
-def filter(vocau, stoplist):
+def filter(vocau, stoplist, contents):
+    count = 0
     for x in xrange(len(vocau)):
-        print 'create thread'
-        t = threading.Thread(target=diff, args=(vocau[x], stoplist[x], x))
-        t.start()
-    print 'judge thread'
-    t = threading.Thread(target=judge)
-    t.start()
+        if not vocau[x] == [] and not stoplist[x] == []:
+            pos = getPos(vocau[x][0][0])
+            print 'pos', pos
+            count += 1
+            t = threading.Thread(target=diff, args=(contents, vocau[x], stoplist[x], pos))
+            t.start()
+    print 'count:',count
+    ts = threading.Thread(target=listenJ, args=(count,))
+    ts.start()
 
-def judge():
-    while not len(JUDGE) == 26:
+def listenJ(count):
+    while len(COUNTER) != count:
+        print 'COUNTER',
         pass
-    print FEATHER_WORDS
+    print 'END'
+    del COUNTER[:]
 
-def diff(words, stopwords, pos):
-    print 'thread start'
-    result = []
-    if words == [] or stopwords == []:
-        pass
-    else:
-        for x in xrange(len(words)):
-            count = 0
-            for y in xrange(len(stopwords)):
-                if words[x] == stopwords[y]:
-                    count += 1
-            if not count:
-                result.append(words[x])
+def diff(contents, words, stopwords, pos):
+    for x in xrange(len(words)):
+        words[x] = words[x].strip()
+        if stopwords.count(words[x]) == 0:
+            contents[pos].append(words[x])
+    COUNTER.append(1)
+    print 'thread end'
 
-    FEATHER_WORDS.insert(pos, result)
-    JUDGE.append(1)
+def stemmer(wordlist):
+    for x in xrange(len(wordlist)):
+        for y in xrange(len(wordlist[x])):
+            wordlist[x][y] = Stemming.stem(wordlist[x][y])
 
+#first filter
 Words = WordExtra('test.txt')
 StopList = WordExtra('StopList.txt')
-filter(Words, StopList)
+filter(Words, StopList, FEATHER_WORDS)
+
+#stemming
+FEATHER_STEM_WORDS = []
+for x in xrange(26):
+    FEATHER_STEM_WORDS.append([])
+stemmer(Words)
+print 'Second'
+filter(Words, StopList, FEATHER_STEM_WORDS)
+print Words

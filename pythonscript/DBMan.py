802735841050860035b35d436b3e12770605109d
tanghan <tanghanzxp@gmail.com>
2016-07-26 23:30:13 +0800

    2016.07.26

--- pythonscript/DBMan.py
+++ pythonscript/DBMan.py
@@ -1,77 +1,28 @@
-from pythonscript.Database import DBSession,Brain,Abdomen,Iris,Lung_CR,Lung_CT, Picture
+#coding:utf8
+from Database import *
+from sqlalchemy.sql import select
 
-class DBMan:
-    def insertBrain(self, local_addr,net_addr):
-        session = DBSession()
-        try:
-            lung = Brain(local_addr=local_addr, net_addr=net_addr)
-            session.add(lung)
-            session.commit()
-        except Exception, e:
-            print 'insert lung_CR error:', e
-        session.close()
+'''picture表操作'''
+#插入
+#表数据插入
+def picture_insert(pid, sourcetableid, hashstr):
+    try:
+        picture_table=Table("picture", metadata, autoload=True)
+        insert = picture_table.insert()
+        dict = {'pid': pid, 'sourcetableid': sourcetableid, 'hashstr': hashstr}
+        conn.execute(insert, **dict)
+    except Exception, e:
+        print 'picture insert error:', e
+        return False
+#表数据查询
+def picture_getinfo(hashstr):
+    try:
+        sel = select([picture]).where([picture.hashstr == hashstr])
+        print sel
+    except Exception, e:
+        print 'Select error in picture: ', e
+        return False
 
-    def insertLung_CT(self, local_addr,net_addr):
-        session = DBSession()
-        try:
-            lung = Lung_CT(local_addr=local_addr, net_addr=net_addr)
-            session.add(lung)
-            session.commit()
-        except Exception, e:
-            print 'insert lung_CR error:', e
-        session.close()
-
-    def insertLung_CR(self, local_addr,net_addr):
-        session = DBSession()
-        try:
-            lung = Lung_CR(local_addr=local_addr, net_addr=net_addr)
-            session.add(lung)
-            session.commit()
-        except Exception, e:
-            print 'insert lung_CR error:', e
-        session.close()
-
-    def insertIris(self, local_addr,net_addr):
-        session = DBSession()
-        try:
-            pupil = Iris(local_addr=local_addr, net_addr=net_addr)
-            session.add(pupil)
-            session.commit()
-        except Exception, e:
-            print 'insert iris error:', e
-        session.close()
-
-    def insertAbdomen(self, local_addr, net_addr):
-        session = DBSession()
-        try:
-            abdomen = Abdomen(local_addr=local_addr, net_addr=net_addr)
-            session.add(abdomen)
-            session.commit()
-        except Exception, e:
-            print "insert abdomen error:", e
-        session.close()
-
-    def insertPicture(self, local_addr, net_addr, artical_url):
-        session = DBSession()
-        try:
-            picture = Picture(local_addr, net_addr, artical_url)
-            session.add(picture)
-            session.commit()
-        except Exception, e:
-            print 'insert picture error:',e
-        session.close()
-
-    def getallpictures(self):
-        session = DBSession()
-        query = session.query(Picture.artical_url, Picture.local_addr, Picture.net_addr).all()
-        print query
-
-    def articalexist(self, artical_url):
-        session = DBSession()
-        query = session.query(Picture).filter(Picture.artical_url == artical_url).first()
-
-        return query != None
 
 if __name__ == "__main__":
-    dbman = DBMan()
-    print dbman.articalexist('asd')
+    picture_getinfo('1234123412341234123412341234123412341234123412341234123412341234')
--- pythonscript/Database.py
+++ pythonscript/Database.py
@@ -1,90 +1,43 @@
-#coding:utf8
-#file is used to manu database
-#time = 2016.04.24
+# coding:utf8
+# file is used to manu database
+# time = 2016.04.24
 #  #author = 'tanghan'
 from sqlalchemy import ForeignKey
-from sqlalchemy import Column, String, create_engine,Integer
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, create_engine, Integer, Table, MetaData
+
 '''医学图片分类别有'''
 
-#数据库基类
-Base = declarative_base()
-engine = create_engine('mysql+mysqldb://root:tanghan940428@localhost:3306/medicalpic')
-#定义数据库对象
-class Picture(Base):
-    #表名
-    __tablename__ = 'Picture'
+# 数据库基类
+metadata = MetaData()
+engine = create_engine('mysql+mysqldb://root:tanghan@localhost:3306/medicalpic')
 
-    local_addr = Column(String(255), nullable=False, primary_key=True)
-    net_addr = Column(String(255), nullable=False)
-    artical_url = Column(String(255), nullable = False)
+#总表， 供hash值比对
+picture = Table("picture", metadata,
+                Column('id', Integer, autoincrement=True, primary_key=True),
+                Column('pid', Integer, nullable=False),
+                Column('sourcetableid', Integer, ForeignKey('sourcetables.id'), nullable=False),
+                Column('hashstr', String(64), nullable=False)
+                )
 
-    def __init__(self, local_addr, net_addr, artical_url):
-        self.local_addr = local_addr
-        self.net_addr = net_addr
-        self.artical_url = artical_url
+#记录图片存储表
+sourcetables = Table("sourcetables", metadata,
+                     Column('id', Integer, autoincrement=True, primary_key=True),
+                     Column("tablename", String(255), nullable=False, unique=True)
+                     )
 
-class Brain(Base):
-    #表明
-    __tablename__ = 'Brain'
+#xctmr文章url存储表
+xctmr_website = Table("xctmr_website", metadata,
+                      Column('id', Integer, autoincrement=True),
+                      Column('url', Integer, unique=True)
+                      )
 
+#xctmr图片存储表
+xctmr_picture = Table("xctmr_picture", metadata,
+                      Column('id', Integer, autoincrement=True, primary_key=True),
+                      Column('localaddr', String(255), nullable=False, unique=True),
+                      Column('urlid', String(255), nullable=True)
+                      )
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    local_addr = Column(String(128), ForeignKey(Picture.local_addr))
-    net_addr = Column(String(128), nullable=False)
-
-    def __init__(self, local_addr, net_addr):
-        self.local_addr = local_addr
-        self.net_addr = net_addr
-
-class Lung_CR(Base):
-    # 表明
-    __tablename__ = 'Lung_CR'
-
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    local_addr = Column(String(255), ForeignKey(Picture.local_addr))
-    net_addr = Column(String(255), nullable=False)
-
-    def __init__(self, local_addr, net_addr):
-        self.local_addr = local_addr
-        self.net_addr = net_addr
-
-class Lung_CT(Base):
-    # 表明
-    __tablename__ = 'Lung_CT'
-
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    local_addr = Column(String(255), ForeignKey(Picture.local_addr))
-    net_addr = Column(String(255), nullable=False)
-
-    def __init__(self, local_addr, net_addr):
-        self.local_addr = local_addr
-        self.net_addr = net_addr
-
-class Iris(Base):
-    # 表明
-    __tablename__ = 'Iris'
-
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    local_addr = Column(String(255), ForeignKey(Picture.local_addr))
-    net_addr = Column(String(255), nullable=False)
-
-    def __init__(self, local_addr, net_addr):
-        self.local_addr = local_addr
-        self.net_addr = net_addr
-
-class Abdomen(Base):
-    # 表明
-    __tablename__ = 'Abdomen'
-
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    local_addr = Column(String(255), ForeignKey(Picture.local_addr))
-    net_addr = Column(String(255), nullable=False)
-
-    def __init__(self, local_addr, net_addr):
-        self.local_addr = local_addr
-        self.net_addr = net_addr
-
-#初始化数据库连接
-DBSession = sessionmaker(bind=engine)
+metadata.create_all(engine)
+# 初始化数据库连接
+conn = engine.connect()

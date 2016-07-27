e59c93878edec5a1435072e0de93d808b5191d1d
tanghan <tanghanzxp@gmail.com>
2016-07-27 18:19:14 +0800

    2016.07.27

--- pythonscript/DBMan.py
+++ pythonscript/DBMan.py
@@ -1,28 +1,258 @@
 #coding:utf8
+from sqlalchemy.orm import Mapper
+
 from Database import *
 from sqlalchemy.sql import select
+
+'''xctmr_website 表操作'''
+# 插入
+def xctmrwebsite_insert(url):
+    try:
+        session = DBSession()
+        website = xctmr_Website(url=url)
+        session.add(website)
+        session.commit()
+        session.close()
+    except Exception, e:
+        print 'Error in insert xctmr_website:', e
+        return False
+    return True
+#查询
+#url存在
+def xctmrwebsite_urlexist(url):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_Website.id).filter(xctmr_Website.url == url).fetchone()
+        session.close()
+        return not result == None
+    except Exception, e:
+        print 'Error in judge whether url exist:', e
+        return False
+#id存在
+def xctmrwebsite_idexist(id):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_Website.id).filter(xctmr_Website.id == id).fetchone()
+        return not result == None
+    except Exception,e:
+        print 'Error in judge id exist in xctmr_website:', e
+        return False
+
+# id获取url
+def xctmrwebsite_getidbyurl(url):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_Website.id).filter(xctmr_Website.url == url).fetchone()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error in get id by url on xctmr_website:', e
+        return None
+# url获取id
+def xctmrwebsite_geturlbyid(id):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_Website.url).filter(xctmr_Website.id == id).fetchone()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error in get url by id on xctmr_website:', e
+        return None
+
+'''xctmr_picture表操作'''
+#插入
+def xctmrpicture_insert(localaddr, urlid):
+    try:
+        session = DBSession()
+        xctmrpicture = xctmr_picture(localaddr=localaddr, urlid=urlid)
+        session.add(xctmrpicture)
+        session.commit()
+        session.close()
+    except Exception, e:
+        print 'Error in insert into xctmr_picture:', e
+        return False
+    return True
+
+# 查询
+# id exist
+def xctmrpicture_idexist(id):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_picture.id).filter(xctmr_picture.id == id).fetchone()
+        session.close()
+        return not result == None
+    except Exception, e:
+        print 'Error on xctmr_picture id exist:', e
+        return False
+
+# localaddr exist
+def xctmrpicture_localaddrexist(localaddr):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_picture.id).filter(xctmr_picture.localaddr == localaddr).fetchone()
+        session.close()
+        return not result == None
+    except Exception, e:
+        print 'Error on xctmr_picture localaddr exist:', e
+        return False
+
+# 查询
+def xctmrpicture_getidbylocaladdr(localaddr):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_picture.id).filter(xctmr_picture.localaddr == localaddr).fetchone()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error on xctmr_picture localaddr exist:', e
+        return None
+
+def xctmrpicture_getlocaladdrbyid(id):
+    try:
+        session = DBSession()
+        result = session.query(xctmr_picture.localaddr).filter(xctmr_picture.id == id).fetchone()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error on xctmr_picture localaddr exist:', e
+        return None
+
+'''sourcetables表操作'''
+def sourcetables_insert(tablename):
+    try:
+        session = DBSession()
+        t = sourcetables(tablename=tablename)
+        session.add(t)
+        session.commit()
+        session.close()
+    except Exception, e:
+        print 'sourcetables insert error:', e
+        return False
+    return True
+#查询
+# id exist
+def sourcetables_idexist(id):
+    try:
+        session = DBSession()
+        result = session.query(sourcetables.id).filter(sourcetables.id == id).fetchone()
+        session.close()
+        return not result == None
+    except Exception, e:
+        print 'Error in sourcetables_idexist:', e
+        return False
+
+# tablename exist
+def sourcetables_tablenameexist(tablename):
+    try:
+        session = DBSession()
+        result = session.query(sourcetables.id).filter(sourcetables.tablename == tablename).fetchone()
+        session.close()
+        return not result == None
+    except Exception, e:
+        print 'Error in sourcetables_tablenameexist:', e
+        return False
+
+# get id by tablename
+def sourcetabls_getidbytablename(tablename):
+    try:
+        session = DBSession()
+        result = session.query(sourcetables.id).filter(sourcetables.tablename == tablename).fetchone()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error in getidbytablename:', e
+        return None
+
+# get tablename by id
+def sourcetable_gettablenamebyid(id):
+    try:
+        session = DBSession()
+        result = session.query(sourcetables.tablename).filter(sourcetables.id == id).fetchone()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error in gettablenamebyid:', e
+        return None
+
 
 '''picture表操作'''
 #插入
 #表数据插入
 def picture_insert(pid, sourcetableid, hashstr):
     try:
-        picture_table=Table("picture", metadata, autoload=True)
-        insert = picture_table.insert()
-        dict = {'pid': pid, 'sourcetableid': sourcetableid, 'hashstr': hashstr}
-        conn.execute(insert, **dict)
+        session = DBSession()
+        pic = picture(pid=pid, sourcetableid=sourcetableid, hashstr=hashstr)
+        session.add(pic)
+        session.commit()
+        session.close()
     except Exception, e:
-        print 'picture insert error:', e
+        print 'Error in insert picture:', e
         return False
-#表数据查询
-def picture_getinfo(hashstr):
+    return True
+
+#查询
+# id exist
+def picture_idexist(id):
     try:
-        sel = select([picture]).where([picture.hashstr == hashstr])
-        print sel
+        session = DBSession()
+        result = session.query(picture.id).filter(picture.id == id).fetchone()
+        return not result == None
     except Exception, e:
-        print 'Select error in picture: ', e
+        print 'Error in picture_idexist:', e
         return False
 
+# pid exist
+def picture_pidexist(pid):
+    try:
+        session = DBSession()
+        result = session.query(picture.id).filter(picture.pid == pid).fetchone()
+        return not result == None
+    except Exception, e:
+        print 'Error in picture_pidexist:', e
+        return False
+
+# sourcetableid exist
+def picture_sourcetableidexist(sourcetableid):
+    try:
+        session = DBSession()
+        result = session.query(picture.id).filter(picture.sourcetableid == sourcetableid).fetchone()
+        return not result == None
+    except Exception, e:
+        print 'Error in picture_sourcetableidexist:', e
+        return False
+
+# hashstr exist
+def picture_hashstrexist(hashstr):
+    try:
+        session = DBSession()
+        result = session.query(picture.id).filter(picture.hashstr == hashstr).fetchone()
+        return not result == None
+    except Exception, e:
+        print 'Error in picture_hashstrexist:', e
+        return False
+
+
+# get info by hashstr
+def picture_getinfobyhashstr(hashstr):
+    try:
+        session = DBSession()
+        result = session.query(picture.id, picture.pid, picture.sourcetableid, picture.hashstr).filter(picture.hashstr == hashstr).fetchall()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error in picture_getinfobyhashstr:', e
+        return []
+
+# get info by id
+def picture_getinfobyid(id):
+    try:
+        session = DBSession()
+        result = session.query(picture.id, picture.pid, picture.sourcetableid, picture.hashstr).filter(picture.id == id).fetchall()
+        session.close()
+        return result
+    except Exception, e:
+        print 'Error in picture_getinfobyhashstr:', e
+        return []
 
 if __name__ == "__main__":
-    picture_getinfo('1234123412341234123412341234123412341234123412341234123412341234')
+    xctmrwebsite_insert('123')
--- pythonscript/Database.py
+++ pythonscript/Database.py
@@ -3,41 +3,61 @@
 # time = 2016.04.24
 #  #author = 'tanghan'
 from sqlalchemy import ForeignKey
-from sqlalchemy import Column, String, create_engine, Integer, Table, MetaData
+from sqlalchemy import Column, String, create_engine, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 '''医学图片分类别有'''
 
 # 数据库基类
-metadata = MetaData()
 engine = create_engine('mysql+mysqldb://root:tanghan@localhost:3306/medicalpic')
-
+Base = declarative_base(bind=engine)
 #总表， 供hash值比对
-picture = Table("picture", metadata,
-                Column('id', Integer, autoincrement=True, primary_key=True),
-                Column('pid', Integer, nullable=False),
-                Column('sourcetableid', Integer, ForeignKey('sourcetables.id'), nullable=False),
-                Column('hashstr', String(64), nullable=False)
-                )
+class picture(Base):
+    __tablename__ = 'picture'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    pid = Column(Integer, nullable=False)
+    sourcetableid = Column(Integer, ForeignKey('sourcetables.id'), nullable=False)
+    hashstr = Column(String(64), nullable=False)
+
+    def __init__(self, pid, sourcetableid, hashstr):
+        self.pid = pid
+        self.sourcetableid = sourcetableid
+        self.hashstr = hashstr
 
 #记录图片存储表
-sourcetables = Table("sourcetables", metadata,
-                     Column('id', Integer, autoincrement=True, primary_key=True),
-                     Column("tablename", String(255), nullable=False, unique=True)
-                     )
+class sourcetables(Base):
+    __tablename__ = 'sourcetables'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tablename = Column(String(255), nullable=False, unique=True)
+
+    def __init__(self, tablename):
+        self.tablename = tablename
+
 
 #xctmr文章url存储表
-xctmr_website = Table("xctmr_website", metadata,
-                      Column('id', Integer, autoincrement=True),
-                      Column('url', Integer, unique=True)
-                      )
+class xctmr_Website(Base):
+    __tablename__ = "xctmr_website"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    url = Column(Integer, unique=True)
+
+    def __init__(self, url):
+        self.url = url
 
 #xctmr图片存储表
-xctmr_picture = Table("xctmr_picture", metadata,
-                      Column('id', Integer, autoincrement=True, primary_key=True),
-                      Column('localaddr', String(255), nullable=False, unique=True),
-                      Column('urlid', String(255), nullable=True)
-                      )
+class xctmr_picture(Base):
+    __tablename__ = "xctmr_picture"
 
-metadata.create_all(engine)
-# 初始化数据库连接
-conn = engine.connect()
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    localaddr = Column(String(255), nullable=False, unique=True)
+    urlid = Column(String(255), nullable=True)
+
+    def __init__(self, localaddr, urlid):
+        self.localaddr = localaddr
+        self.urlid = urlid
+
+DBSession = sessionmaker()
+Base.metadata.create_all(engine)
